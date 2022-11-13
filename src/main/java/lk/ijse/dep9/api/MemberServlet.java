200c2ba7e7b87d5ce3221adde882b73c43191d61
Eranga Bandara <erangabandara94@gmail.com>
2022-11-13 10:00:48 +0530

    Setup the connection pool

--- src/main/java/lk/ijse/dep9/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep9/api/MemberServlet.java
@@ -1,18 +1,32 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.util.HttpServlet2;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
 @WebServlet(name = "MemberServlet", value = "/members/*", loadOnStartup = 0)
 public class MemberServlet extends HttpServlet2 {
+
+    @Resource(lookup = "java:comp/env/jdbc/lms")
+    private DataSource pool;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("members doGet()");
     }
+
+
+
+
+//    Load all members - GET /members
+//    Load members by page - GET /members?size={size}&page={page}
+//    Search members - GET /members?q={query}
+//    Search members by page- GET /members?q={query}&size{size}&page={page}
+//    get member details - GET /members/{member-id}
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
